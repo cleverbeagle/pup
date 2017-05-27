e56cd9cf809f1b3d9be23ce726d023fa44fe7a27
cleverbeagle <business@cleverbeagle.com>
2017-05-27 15:34:21 -0500

    add support for user and document fixtures

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -2,32 +2,53 @@ import seeder from '@cleverbeagle/seeder';
 import { Meteor } from 'meteor/meteor';
 import Documents from '../../api/Documents/Documents';
 
+const documentsSeed = userId => ({
+  collection: Documents,
+  environments: ['development', 'staging'],
+  noLimit: true,
+  modelCount: 5,
+  model(dataIndex) {
+    return {
+      owner: userId,
+      title: `Document #${dataIndex + 1}`,
+      body: `This is the body of document #${dataIndex + 1}`,
+    };
+  },
+});
+
 seeder(Meteor.users, {
   environments: ['development', 'staging'],
+  noLimit: true,
   data: [{
     email: 'admin@admin.com',
     password: 'password',
     profile: {
       name: {
-        first: 'George',
-        last: 'Clooney',
+        first: 'Andy',
+        last: 'Warhol',
       },
     },
     roles: ['admin'],
+    data(userId) {
+      return documentsSeed(userId);
+    },
   }],
-});
-
-seeder(Documents, {
-  environments: ['development', 'staging'],
-  wipe: true,
-  min: 10,
+  modelCount: 5,
   model(index, faker) {
-    const owner = Meteor.users.findOne({ 'emails.address': 'admin@admin.com' })._id;
-    const documentNumber = index + 1;
+    const userCount = index + 1;
     return {
-      owner,
-      title: `${faker.lorem.words(5)} #${documentNumber}`,
-      body: `This is the body of example document #${documentNumber}.`,
+      email: `user+${userCount}@test.com`,
+      password: 'password',
+      profile: {
+        name: {
+          first: faker.name.firstName(),
+          last: faker.name.lastName(),
+        },
+      },
+      roles: ['user'],
+      data(userId) {
+        return documentsSeed(userId);
+      },
     };
   },
 });
