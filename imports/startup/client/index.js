a9ba6c60bdc158ee4ffdfa9d70d8d5d53fe59f20
zodern <zodern@users.noreply.github.com>
2017-10-23 11:45:58 -0500

    Import methods on client

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App/App';
+import '../api';
+import './api';
 
 import '../../ui/stylesheets/app.scss';
 
