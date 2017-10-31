c580270f61f8f2384dd283fe48d648f6aa364449
zodern <zodern@users.noreply.github.com>
2017-10-31 16:09:37 -0500

    Move shared api to the startup/both folder

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App/App';
-import '../api';
+import '../both/api';
 import './api';
 
 import '../../ui/stylesheets/app.scss';
