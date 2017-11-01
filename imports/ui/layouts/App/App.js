8eb65f357fa1fd37af6a730534352f34cacee496
Cenk Dolek <cdolek@gmail.com>
2017-11-01 13:50:36 -0400

    Update deprecated react-meteor-data "createContainer" with "withTracker"

--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -1,11 +1,11 @@
-/* eslint-disable jsx-a11y/no-href*/
+/* eslint-disable jsx-a11y/no-href */
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Grid, Alert, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import { Bert } from 'meteor/themeteorchef:bert';
 import Navigation from '../../components/Navigation/Navigation';
@@ -43,31 +43,51 @@ const handleResendVerificationEmail = (emailAddress) => {
 
 const App = props => (
   <Router>
-    {!props.loading ? <div className="App">
-      {props.userId && !props.emailVerified ? <Alert className="verify-email text-center"><p>Hey friend! Can you <strong>verify your email address</strong> ({props.emailAddress}) for us? <Button bsStyle="link" onClick={() => handleResendVerificationEmail(props.emailAddress)} href="#">Re-send verification email</Button></p></Alert> : ''}
-      <Navigation {...props} />
-      <Grid>
-        <Switch>
-          <Route exact name="index" path="/" component={Index} />
-          <Authenticated exact path="/documents" component={Documents} {...props} />
-          <Authenticated exact path="/documents/new" component={NewDocument} {...props} />
-          <Authenticated exact path="/documents/:_id" component={ViewDocument} {...props} />
-          <Authenticated exact path="/documents/:_id/edit" component={EditDocument} {...props} />
-          <Authenticated exact path="/profile" component={Profile} {...props} />
-          <Public path="/signup" component={Signup} {...props} />
-          <Public path="/login" component={Login} {...props} />
-          <Route path="/logout" component={Logout} {...props} />
-          <Route name="verify-email" path="/verify-email/:token" component={VerifyEmail} />
-          <Route name="recover-password" path="/recover-password" component={RecoverPassword} />
-          <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
-          <Route name="terms" path="/terms" component={Terms} />
-          <Route name="privacy" path="/privacy" component={Privacy} />
-          <Route name="examplePage" path="/example-page" component={ExamplePage} />
-          <Route component={NotFound} />
-        </Switch>
-      </Grid>
-      <Footer />
-    </div> : ''}
+    {!props.loading ? (
+      <div className="App">
+        {props.userId && !props.emailVerified ? (
+          <Alert className="verify-email text-center">
+            <p>
+              Hey friend! Can you <strong>verify your email address</strong> ({props.emailAddress})
+              for us?{' '}
+              <Button
+                bsStyle="link"
+                onClick={() => handleResendVerificationEmail(props.emailAddress)}
+                href="#"
+              >
+                Re-send verification email
+              </Button>
+            </p>
+          </Alert>
+        ) : (
+          ''
+        )}
+        <Navigation {...props} />
+        <Grid>
+          <Switch>
+            <Route exact name="index" path="/" component={Index} />
+            <Authenticated exact path="/documents" component={Documents} {...props} />
+            <Authenticated exact path="/documents/new" component={NewDocument} {...props} />
+            <Authenticated exact path="/documents/:_id" component={ViewDocument} {...props} />
+            <Authenticated exact path="/documents/:_id/edit" component={EditDocument} {...props} />
+            <Authenticated exact path="/profile" component={Profile} {...props} />
+            <Public path="/signup" component={Signup} {...props} />
+            <Public path="/login" component={Login} {...props} />
+            <Route path="/logout" component={Logout} {...props} />
+            <Route name="verify-email" path="/verify-email/:token" component={VerifyEmail} />
+            <Route name="recover-password" path="/recover-password" component={RecoverPassword} />
+            <Route name="reset-password" path="/reset-password/:token" component={ResetPassword} />
+            <Route name="terms" path="/terms" component={Terms} />
+            <Route name="privacy" path="/privacy" component={Privacy} />
+            <Route name="examplePage" path="/example-page" component={ExamplePage} />
+            <Route component={NotFound} />
+          </Switch>
+        </Grid>
+        <Footer />
+      </div>
+    ) : (
+      ''
+    )}
   </Router>
 );
 
@@ -83,12 +103,13 @@ App.propTypes = {
   emailVerified: PropTypes.bool.isRequired,
 };
 
-const getUserName = name => ({
-  string: name,
-  object: `${name.first} ${name.last}`,
-}[typeof name]);
+const getUserName = name =>
+  ({
+    string: name,
+    object: `${name.first} ${name.last}`,
+  }[typeof name]);
 
-export default createContainer(() => {
+export default withTracker(() => {
   const loggingIn = Meteor.loggingIn();
   const user = Meteor.user();
   const userId = Meteor.userId();
@@ -106,4 +127,4 @@ export default createContainer(() => {
     emailAddress,
     emailVerified: user && user.emails ? user && user.emails && user.emails[0].verified : true,
   };
-}, App);
+})(App);
