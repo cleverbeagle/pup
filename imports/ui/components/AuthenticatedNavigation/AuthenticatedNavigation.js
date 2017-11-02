f449d1dc6649467eb08f5821c5ca83807b62d26e
Cenk Dolek <cdolek@gmail.com>
2017-11-02 11:26:02 -0400

    removed unused import { Meteor } + added history propType as required

--- imports/ui/components/AuthenticatedNavigation/AuthenticatedNavigation.js
+++ imports/ui/components/AuthenticatedNavigation/AuthenticatedNavigation.js
@@ -3,22 +3,27 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { Meteor } from 'meteor/meteor';
 
 const AuthenticatedNavigation = ({ name, history }) => (
   <div>
     <Nav>
       <LinkContainer to="/documents">
-        <NavItem eventKey={1} href="/documents">Documents</NavItem>
+        <NavItem eventKey={1} href="/documents">
+          Documents
+        </NavItem>
       </LinkContainer>
     </Nav>
     <Nav pullRight>
       <NavDropdown eventKey={2} title={name} id="user-nav-dropdown">
         <LinkContainer to="/profile">
-          <NavItem eventKey={2.1} href="/profile">Profile</NavItem>
+          <NavItem eventKey={2.1} href="/profile">
+            Profile
+          </NavItem>
         </LinkContainer>
         <MenuItem divider />
-        <MenuItem eventKey={2.2} onClick={() => history.push('/logout')}>Logout</MenuItem>
+        <MenuItem eventKey={2.2} onClick={() => history.push('/logout')}>
+          Logout
+        </MenuItem>
       </NavDropdown>
     </Nav>
   </div>
@@ -26,6 +31,7 @@ const AuthenticatedNavigation = ({ name, history }) => (
 
 AuthenticatedNavigation.propTypes = {
   name: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default withRouter(AuthenticatedNavigation);
