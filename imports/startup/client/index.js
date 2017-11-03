9d32f203a2585f5c29b6b37bdc0d03582c35438b
cleverbeagle <business@cleverbeagle.com>
2017-11-03 14:55:39 -0500

    drop unnecessary /imports/startup/client/api.js file

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App/App';
 import '../both/api';
-import './api';
 
 import '../../ui/stylesheets/app.scss';
 
