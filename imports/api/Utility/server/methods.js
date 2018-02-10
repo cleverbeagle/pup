7b67a02ff50b37a13420096b3874b01e13df0cd8
sw-yx <swyx@swyx.io>
2018-02-10 12:27:10 -0500

    refactor markdown pages to use react-markdown

--- imports/api/Utility/server/methods.js
+++ imports/api/Utility/server/methods.js
@@ -1,11 +1,13 @@
-import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
-import getPrivateFile from '../../../modules/server/get-private-file';
-import parseMarkdown from '../../../modules/parse-markdown';
+// // you can write serverside methods here, e.g.: Meteor.methods({})
 
-Meteor.methods({
-  'utility.getPage': function utilityGetPage(fileName) {
-    check(fileName, String);
-    return parseMarkdown(getPrivateFile(`pages/${fileName}.md`));
-  },
-});
+// import { Meteor } from 'meteor/meteor';
+// import { check } from 'meteor/check';
+// import getPrivateFile from '../../../modules/server/get-private-file';
+// import parseMarkdown from '../../../modules/parse-markdown';
+
+// Meteor.methods({
+//   'utility.getPage': function utilityGetPage(fileName) {
+//     check(fileName, String);
+//     return parseMarkdown(getPrivateFile(`pages/${fileName}.md`));
+//   },
+// });
--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -5,4 +5,4 @@ import '../../api/OAuth/server/methods';
 import '../../api/Users/server/methods';
 import '../../api/Users/server/publications';
 
-import '../../api/Utility/server/methods';
+// import '../../api/Utility/server/methods'; // uncomment to use this file
