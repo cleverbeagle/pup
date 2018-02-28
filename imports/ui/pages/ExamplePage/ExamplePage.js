d6753df5fe1ad64676703c453887000f769b5f55
cleverbeagle <business@cleverbeagle.com>
2018-02-28 14:12:38 -0600

    fix missing changes to static pages feature

--- imports/ui/pages/ExamplePage/ExamplePage.js
+++ imports/ui/pages/ExamplePage/ExamplePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Page from '../Page/Page';
 
+const content = `
+### This is my Markdown page
+I can type **any** Markdown I want into this file and it will ultimately be parsed into HTML by the &lt;Page /&gt; component in the source of this page.
+`;
+
 const ExamplePage = () => (
   <div className="ExamplePage">
     <Page
       title="My Example Page"
       subtitle="A subtitle for my example page."
-      page="example-page"
+      content={content}
     />
   </div>
 );
