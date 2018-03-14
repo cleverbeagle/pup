1dd87906c6b8d38797c148a90e2001b1c9e659b9
Toine van Kampen <toine.vankampen@gmail.com>
2018-03-14 22:59:42 +1000

    Guard Startup against missing settings file
    Missing settings file will now log a warning, the   server starts up
    without issue.

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -1,3 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
-if (Meteor.isDevelopment) process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+if (Meteor.settings.private && Meteor.settings.private.MAIL_URL) {
+  if (Meteor.isDevelopment) process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+} else {
+  console.warn('Warning! You have not configured mail settings. Emails will not be sent.');
+}
