2d3ab9adda26c71c17ba1ff48a568617dc470ccb
Merlin Patterson <merlinpatt@gmail.com>
2018-09-02 12:26:20 -0400

    Update browser policy for fontawesome

--- startup/server/browserPolicy.js
+++ startup/server/browserPolicy.js
@@ -1,3 +1,5 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
 // e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+BrowserPolicy.content.allowOriginForAll('https://use.fontawesome.com/releases/v5.0.10/css/all.css');
 BrowserPolicy.content.allowFontOrigin('data:');
+BrowserPolicy.content.allowFontOrigin('https://use.fontawesome.com/');
