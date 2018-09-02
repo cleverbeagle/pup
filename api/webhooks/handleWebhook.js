dc2d357f73a6df70002c8bde283068133deb1078
Merlin Patterson <merlinpatt@gmail.com>
2018-09-02 10:16:58 -0400

    Add basic webhooks handler

---
+++ api/webhooks/handleWebhook.js
@@ -0,0 +1,12 @@
+// import service handlers here
+
+export default {
+  // nameOfService(request) {
+  //   // get type of request, different services will have different ways to get the type
+  //   const type = request.body.type;
+  //   // get specific service handler and use it
+  //   const handler = stripe[type];
+  //   if (handler) return handler(request.body);
+  //   return `${type} is not supported.`;
+  // },
+};
--- startup/server/api.js
+++ startup/server/api.js
@@ -10,3 +10,5 @@ import '../../api/OAuth/server/methods';
 
 import '../../api/Users/server/methods';
 import '../../api/Users/server/publications';
+
+import '../../api/webhooks';
