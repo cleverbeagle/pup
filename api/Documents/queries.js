b7fa4299154c3e03a3ed79d2a97e5d1415fc5544
cleverbeagle <business@cleverbeagle.com>
2018-09-06 14:39:19 -0500

    add queries I accidentally deleted

--- api/Documents/queries.js
+++ api/Documents/queries.js
@@ -8,3 +8,13 @@ export default {
   },
   document: (parent, args, { db }) => db.mongodb.collection('Documents').findOne({ _id: args._id }),
 };
+export default {
+  documents: (parent, args, { user, db }) => {
+    const query = Object.assign({}, args, user && user._id ? { owner: user._id } : {});
+    return db.mongodb
+      .collection('Documents')
+      .find(query)
+      .toArray();
+  },
+  document: (parent, args, { db }) => db.mongodb.collection('Documents').findOne({ _id: args._id }),
+};
