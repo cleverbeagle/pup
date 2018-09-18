557898bb3795de71fe703053c55537663633013a
Merlin Patterson <merlinpatt@gmail.com>
2018-09-18 14:43:12 -0400

    Remove dependency on twbs:bootstrap / jQuery 2

--- startup/server/browserPolicy.js
+++ startup/server/browserPolicy.js
@@ -1,5 +1,10 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
-// e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
+
+BrowserPolicy.content.allowOriginForAll(
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+);
+BrowserPolicy.content.allowFontOrigin('https://maxcdn.bootstrapcdn.com/');
+
 BrowserPolicy.content.allowOriginForAll('https://use.fontawesome.com/releases/v5.0.10/css/all.css');
 BrowserPolicy.content.allowFontOrigin('data:');
 BrowserPolicy.content.allowFontOrigin('https://use.fontawesome.com/');
