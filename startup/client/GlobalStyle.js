d707fee9470f75af98256445b590b483d6cbc6d0
Merlin Patterson <merlinpatt@gmail.com>
2018-10-26 10:38:20 -0400

    Upgrade styled components

---
+++ startup/client/GlobalStyle.js
@@ -0,0 +1,96 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary: #337ab7;
+    --success: #5cb85c;
+    --info: #5bc0de;
+    --warning: #f0ad4e;
+    --danger: #d9534f;
+
+    --gray-darker: #222;
+    --gray-dark: #333;
+    --gray: #555;
+    --gray-light: #777;
+    --gray-lighter: #eee;
+
+    --facebook: #3b5998;
+    --google: #ea4335;
+    --github: var(--gray-dark);
+
+    --cb-blue: #4285F4;
+    --cb-green: #00D490;
+    --cb-yellow: #FFCF50;
+    --cb-red: #DA5847;
+  }
+
+  html {
+    position: relative;
+    min-height: 100%;
+  }
+
+  body {
+    margin-bottom: 80px;
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    line-height: 20px;
+  }
+
+  body.isViewDocument {
+    padding-top: 20px;
+  }
+
+  body.isViewDocument .navbar {
+    display: none;
+  }
+
+  .navbar {
+    border-radius: 0;
+    border-left: none;
+    border-right: none;
+    border-top: none;
+  }
+
+  form label {
+    display: block;
+  }
+
+  form .control-label {
+    display: block;
+    margin-bottom: 7px;
+  }
+
+  form label.error {
+    display: block;
+    margin-top: 8px;
+    font-size: 13px;
+    font-weight: normal;
+    color: var(--danger);
+  }
+
+  .page-header {
+    margin-top: 0;
+  }
+
+  .table tr td {
+    vertical-align: middle !important;
+  }
+
+  /* Removes unnecessary bottom padding on .container */
+  body > #react-root > div > .container {
+    padding-bottom: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    body.isViewDocument {
+      padding-top: 40px;
+    }
+
+    .page-header {
+      margin-top: 20px;
+    }
+  }
+`;
+
+export default GlobalStyle;
--- startup/client/index.js
+++ startup/client/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import { ThemeProvider, injectGlobal } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -11,6 +11,7 @@ import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
 import App from '../../ui/layouts/App';
+import GlobalStyle from './GlobalStyle';
 import '../both/api';
 
 Bert.defaults.style = 'growl-bottom-right';
@@ -28,103 +29,11 @@ const apolloClient = new ApolloClient({
 
 Accounts.onLogout(() => apolloClient.resetStore());
 
-injectGlobal`
-  :root {
-    --primary: #337ab7;
-    --success: #5cb85c;
-    --info: #5bc0de;
-    --warning: #f0ad4e;
-    --danger: #d9534f;
-
-    --gray-darker: #222;
-    --gray-dark: #333;
-    --gray: #555;
-    --gray-light: #777;
-    --gray-lighter: #eee;
-
-    --facebook: #3b5998;
-    --google: #ea4335;
-    --github: var(--gray-dark);
-
-    --cb-blue: #4285F4;
-    --cb-green: #00D490;
-    --cb-yellow: #FFCF50;
-    --cb-red: #DA5847;
-  }
-
-  html {
-    position: relative;
-    min-height: 100%;
-  }
-
-  body {
-    margin-bottom: 80px;
-    margin: 0;
-    padding: 0;
-    font-size: 14px;
-    line-height: 20px;
-  }
-
-  body.isViewDocument {
-    padding-top: 20px;
-  }
-
-  body.isViewDocument .navbar {
-    display: none;
-  }
-
-  .navbar {
-    border-radius: 0;
-    border-left: none;
-    border-right: none;
-    border-top: none;
-  }
-
-  form label {
-    display: block;
-  }
-
-  form .control-label {
-    display: block;
-    margin-bottom: 7px;
-  }
-
-  form label.error {
-    display: block;
-    margin-top: 8px;
-    font-size: 13px;
-    font-weight: normal;
-    color: var(--danger);
-  }
-
-  .page-header {
-    margin-top: 0;
-  }
-
-  .table tr td {
-    vertical-align: middle !important;
-  }
-
-  /* Removes unnecessary bottom padding on .container */
-  body > #react-root > div > .container {
-    padding-bottom: 0;
-  }
-
-  @media screen and (min-width: 768px) {
-    body.isViewDocument {
-      padding-top: 40px;
-    }
-
-    .page-header {
-      margin-top: 20px;
-    }
-  }
-`;
-
 Meteor.startup(() =>
   hydrate(
     <ThemeProvider theme={{}}>
       <ApolloProvider client={apolloClient}>
+        <GlobalStyle />
         <BrowserRouter>
           <Switch>
             <App />
