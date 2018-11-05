19e95f1bf486715761bb908f00698f3a3efa3df4
cleverbeagle <business@cleverbeagle.com>
2018-11-05 08:27:09 -0600

    bump meteor and atmosphere packages to latest versions

--- startup/server/browserPolicy.js
+++ startup/server/browserPolicy.js
@@ -1,15 +1,12 @@
 import { BrowserPolicy } from 'meteor/browser-policy-common';
 
 // Bootstrap
-BrowserPolicy.content.allowOriginForAll(
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-);
-BrowserPolicy.content.allowFontOrigin('https://maxcdn.bootstrapcdn.com/');
+BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
 
 // FontAwesome
-BrowserPolicy.content.allowOriginForAll('https://use.fontawesome.com/releases/v5.0.10/css/all.css');
-BrowserPolicy.content.allowFontOrigin('https://use.fontawesome.com/');
+BrowserPolicy.content.allowOriginForAll('use.fontawesome.com');
 
 // Replace these with your own content URLs
-BrowserPolicy.content.allowOriginForAll('http://cleverbeagle-assets.s3.amazonaws.com/');
+BrowserPolicy.content.allowOriginForAll('cleverbeagle-assets.s3.amazonaws.com');
+BrowserPolicy.content.allowOriginForAll('s3-us-west-2.amazonaws.com');
 BrowserPolicy.content.allowFontOrigin('data:');
