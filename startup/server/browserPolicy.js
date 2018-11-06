ebb6de469beb2b345a52d0fadb2263a05159f9c4
cleverbeagle <business@cleverbeagle.com>
2018-11-06 16:18:06 -0600

    add browser policy settings for graphql playground

--- startup/server/browserPolicy.js
+++ startup/server/browserPolicy.js
@@ -6,6 +6,10 @@ BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
 // FontAwesome
 BrowserPolicy.content.allowOriginForAll('use.fontawesome.com');
 
+// GraphQL Playground
+BrowserPolicy.content.allowOriginForAll('graphcool-playground.netlify.com');
+BrowserPolicy.content.allowOriginForAll('cdn.jsdelivr.net');
+
 // Replace these with your own content URLs
 BrowserPolicy.content.allowOriginForAll('cleverbeagle-assets.s3.amazonaws.com');
 BrowserPolicy.content.allowOriginForAll('s3-us-west-2.amazonaws.com');
